4f459f0c0ad7a9113ae94317249aae5e2eb9d4d4
Vadim Vinokurov <vinokur.83@mail.ru>
2020-06-04 23:57:51 +0500

    first commit update

--- listGroupUser.py
+++ listGroupUser.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from pprint import pprint
+
 class User:
 
 	def __init__(self, user1):
@@ -14,28 +15,25 @@ class User:
 			'user_ids': self.user1,
 			'v': '5.107'
 		}
-		try:
-			r = requests.get(URL, params).json()
-		except:
-			pass
-		return r['response'][0]['id'] # ID 171691064
+		r = requests.get(URL, params).json()
+		return r['response'][0]['id']  # ID 171691064
+
+
 
 	def get_groups(self):
-		"""ПОЛУЧАЕМ СПИСОК ГРУПП В КОТОРЫХ СОСТОИТ ПОЛЬЗОВАТЕЛЬ"""
-		list_groups = {}
+		"""ПОЛУЧАЕМ ID ГРУПП В КОТОРЫХ СОСТОИТ ПОЛЬЗОВАТЕЛЬ"""
+		list_groups = []
 		URL = 'https://api.vk.com/method/groups.get?'
 		params = {'user_id':self.get_user(),
 		          'extended':1,
 		          'access_token': '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008',
 		          'fields':'links',
 		          'v': '5.107'}
-		try:
-			r = requests.get(URL, params).json()
-			for count_groups, name_group in enumerate(r['response']['items']):
-				list_groups[count_groups] = f"https://vk.com/{name_group['screen_name']}"
-		except:
-			pass
-		return list_groups # ФУНКЦИЯ ВОЗВРАЩАЕТ ССЫЛКИ НА ГРУППЫ
+		r = requests.get(URL, params).json()
+		for name_group in r['response']['items']:
+			list_groups.append(name_group['id'])
+		return list_groups
+
 
 	def get_friends(self):
 		"""ПОЛУЧАЕМ ДРУЗЕЙ ПОЛЬЗОВАТЕЛЯ"""
@@ -46,12 +44,12 @@ class User:
 			'user_id': self.get_user(),
 			'v': '5.107'
 		}
-		try:
-			r = requests.get(URL, params).json()
-			list_id_friends.append(r['response']['items'])
-		except:
-			pass
+
+		r = requests.get(URL, params).json()
+		list_id_friends.append(r['response']['items'])
 		return list_id_friends
+
+
 
 	def get_groups_friends(self):
 		"""ПОЛУЧАЕМ ГРУППЫ В КОТОРЫХ СОСТОЯТ ДРУЗЬЯ ПОЛЬЗОВАТЕЛЯ"""
@@ -62,22 +60,38 @@ class User:
 		          'access_token': '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008',
 		          'fields': 'links',
 		          'v': '5.107'}
-		try:
-			r = requests.get(URL, params).json()
-			for count_groups, name_group in enumerate(r['response']['items']):
-				list_friends_groups[count_groups] = f"https://vk.com/{name_group['screen_name']}"
-		except:
-			pass
+		r = requests.get(URL, params).json()
+		for count_groups, name_group in enumerate(r['response']['items']):
+			list_friends_groups[count_groups] = f"https://vk.com/{name_group['screen_name']}"
 		return list_friends_groups
 
 
 
+	def get_count_users(self):
+		"""Возвращает список участников сообщества."""
+		d = []
+		URL = 'https://api.vk.com/method/groups.getMembers?'
+		params = {
+			'group_id':'125927592',
+			'sort':'id_asc',
+			'count':10,
+			'fields':'status',
+
+			'access_token':'958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008',
+			'v':'5.107'
+		}
+		r = requests.get(URL, params).json()
+		for i in r['response']['items']:
+			return i
+	#
+	#
+
 user = User(171691064)
 # s = user.get_user()
-# pprint(user.get_groups())
+pprint(user.get_groups())
 # pprint(user.get_user('eshmargunov'))
-# print(user.get_friends())
+# pprint(user.get_friends())
 # pprint(user.get_groups_friends())
-
-if __name__ == '__main__':
-    pprint(user.get_groups_friends())
+pprint(user.get_count_users())
+# if __name__ == '__main__':
+    # pprint(user.get_groups_friends())
